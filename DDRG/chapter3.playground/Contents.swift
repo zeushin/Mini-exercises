7bb4bfa0430dd80c7cb10ddf740b32e71fc0cfa2
RGP_KOREA <rgp_korea@RGP-KOREAui-MacBook-Pro.local>
2016-11-28 19:36:38 +0900

    커미이잇

---
+++ DDRG/chapter3.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//1
+let age1 = 42
+let age2 = 21
+
+//2
+let avg1 = (age1 + age2) / 2
+
+//3
+let avg2 = Double(age1 + age2) / 2
+
+//1
+let firstName:String = "Seulki"
+let lastName:String = "Kim"
+
+//2
+let fullName = firstName + " " + lastName
+
+//3
